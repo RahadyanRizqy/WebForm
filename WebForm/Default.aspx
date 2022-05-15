c0c4a0477d4f4ef9ef0c7dba8387c2a2e208305a
rahad <rahad@MSI>
2022-05-15 08:19:52 +0700

    Add project files.

---
+++ WebForm/Default.aspx
@@ -0,0 +1,99 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForm._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+<table>  
+    <tr>  
+        <td colspan="2">  
+            <h1>Registration Form</h1>  
+        </td>  
+    </tr>
+    <tr>
+        <td>Student ID</td>
+        <td>:</td>
+        <td>
+            <asp:Textbox ID="txtStudID" runat="server"></asp:Textbox>
+        </td>
+    </tr>
+    <tr>  
+        <td>Student Name</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtStudName" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Student Username</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtStudUname" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Student Password</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtStudPword" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Student Email</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtStudEmail" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>
+    <tr>
+        <td>Student Address</td>
+        <td>:</td>
+        <td>
+            <asp:TextBox ID="txtStudAddr" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>  
+        <td colspan="2">  
+            <asp:Button ID="btnInsertion" runat="server" Text="Insert" OnClick="btnInsertion_Click" Style="width: 48px" />
+            <asp:Button ID="btnUpdation" runat="server" Text="Update" OnClick="btnUpdation_Click" />
+            <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>  
+        </td>  
+    </tr>  
+</table>
+<table>  
+    <tr>  
+        <td>  
+            <h1>View Students Data</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>  
+            <asp:Button ID="btnSelection" runat="server" Text="Select All Data" OnClick="btnSelection_Click" />  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>  
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>  
+        </td>  
+    </tr>
+</table>
+<table>  
+    <tr>  
+        <td colspan="3">  
+            <h1>Delete Students Data</h1>  
+        </td>  
+    </tr>  
+    <tr>  
+        <td>Student ID</td>  
+        <td>:</td>  
+        <td>  
+            <asp:TextBox ID="txtStudentID" runat="server"></asp:TextBox>  
+        </td>  
+    </tr>  
+    <tr >  
+        <td colspan="3">  
+            <asp:Button ID="btnDeletion" runat="server" Text="Delete" OnClick="btnDeletion_Click" />  
+            <asp:Label ID="lblmessage" runat="server" ForeColor="Red"></asp:Label>  
+        </td>  
+    </tr>  
+</table>
+
+</asp:Content>
---
+++ WebForm/Default.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using Npgsql;
+
+namespace WebForm
+{
+    public partial class _Default : Page
+    {
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "INSERT INTO students(stud_id, stud_name, stud_uname, stud_pword, stud_email, stud_address) VALUES (@id, @name, @uname, @pword, @email, @address)";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(txtStudID.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@name", txtStudName.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@uname", txtStudUname.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@pword", txtStudPword.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@email", txtStudEmail.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@address", txtStudAddr.Text));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtStudID.Text = "";
+                    txtStudName.Text = "";
+                    txtStudUname.Text = "";
+                    txtStudPword.Text = "";
+                    txtStudEmail.Text = "";
+                    txtStudAddr.Text = "";
+                    lblmsg.Text = "Student data has been registered";
+                }
+
+            }
+            catch (Exception ex) { }
+        }
+        protected void btnSelection_Click(object sender, EventArgs e)
+        {
+            try /* Select After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT * FROM students";
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmd.Dispose();
+                    connection.Close();
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex) { }
+        }
+        protected void btnUpdation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "UPDATE students SET stud_id=@id, stud_name=@name, stud_uname=@uname, stud_pword=@pword, stud_email=@email, stud_address=@address WHERE stud_id=@id";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(txtStudID.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@name", txtStudName.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@uname", txtStudUname.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@pword", txtStudPword.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@email", txtStudEmail.Text));
+                    cmd.Parameters.Add(new NpgsqlParameter("@address", txtStudAddr.Text));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtStudID.Text = "";
+                    txtStudName.Text = "";
+                    txtStudUname.Text = "";
+                    txtStudPword.Text = "";
+                    txtStudEmail.Text = "";
+                    txtStudAddr.Text = "";
+                    lblmsg.Text = "Student data has been modified";
+
+                }
+            }
+            catch (Exception ex) { }
+        }
+        protected void btnDeletion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection())
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "DELETE FROM students WHERE stud_id=@id";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@id", Convert.ToInt32(txtStudentID.Text)));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtStudentID.Text = "";
+                    lblmessage.Text = "Student data has been deleted";
+
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
